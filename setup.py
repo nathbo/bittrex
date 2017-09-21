3e5977f7c44df9662c94d62e37653d87381b4932
nathbo <nathanael.bosch@tum.de>
2017-09-21 12:13:27 +0200

    Created Package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bittrex',
+      version='0.1',
+      description='Simple API wrapper for the Bittrex API',
+      url='http://github.com/nathbo/bittrex',
+      author='Nathanael Bosch',
+      author_email='nathanael.bosch@gmail.com',
+      license='MIT',
+      packages=['bittrex'],
+      zip_safe=False)
